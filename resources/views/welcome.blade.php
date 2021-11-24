e4b395a39c5ccb505a4bcfd442ba691ccf6f8494
Durgesh Pandey <87016571+durgesh1310@users.noreply.github.com>
2021-11-24 21:07:54 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,7 @@
     <body onload="window.location.href = 'https://kite.zerodha.com/connect/login?v=3&api_key=36cu3p0dwq6nyche'">
         <script>
             $(document).ready( function() {
-                window.location.href = "https://kite.zerodha.com/connect/login?v=3&api_key=36cu3p0dwq6nyche"; 
-                
+                window.location.href = "https://kite.zerodha.com/connect/login?v=3&api_key=36cu3p0dwq6nyche";    
             }
         </script>
     </body>
